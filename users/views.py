42f31cbac3fae9a61f186bc91e9ac967c37e265a
Albert Usaqui <ausaqui@4geeks.com.ve>
2017-07-28 02:22:12 -0430

    services to create, update, retrieve and delete provider polygon instances

--- users/views.py
+++ users/views.py
@@ -1,8 +1,14 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import AllowAny
 
 from .models import User
 from .serializers import CreateUserSerializer, UserSerializer
+from polygons.models import ProviderPolygon
+from polygons.serializers import (
+    CreateProviderPolygonSerializer, ProviderPolygonSerializer
+)
 
 
 class UserView(CreateAPIView):
@@ -13,4 +19,21 @@ class UserView(CreateAPIView):
 class UserDetailView(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class ProviderPolygonView(CreateAPIView):
+    serializer_class = CreateProviderPolygonSerializer
+    permission_classes = (AllowAny,)
+
+    def get_serializer_context(self):
+        context = super(ProviderPolygonView, self).get_serializer_context()
+        # TODO: get user instance through authentication process
+        context['user'] = get_object_or_404(User, pk=self.kwargs['pk'])
+        return context
+
+
+class ProviderPolygonDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = ProviderPolygon.objects.all()
+    serializer_class = ProviderPolygonSerializer
     permission_classes = (AllowAny,)
