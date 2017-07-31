50cde5827354d665a24477f679327ae028f3cfdb
Albert Usaqui <ausaqui@4geeks.com.ve>
2017-07-31 12:05:18 -0430

    improving services to handle authenticated users

--- users/views.py
+++ users/views.py
@@ -1,39 +1,71 @@
-from django.shortcuts import get_object_or_404
-
-from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import (
+    ListCreateAPIView, RetrieveUpdateDestroyAPIView
+)
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import User
 from .serializers import CreateUserSerializer, UserSerializer
+
+from moziotest.permissions import (
+    IsOwnerAccountOrReadOnly, IsOwnerObjectOrReadOnly
+)
+
 from polygons.models import ProviderPolygon
 from polygons.serializers import (
     CreateProviderPolygonSerializer, ProviderPolygonSerializer
 )
 
 
-class UserView(CreateAPIView):
+class UserView(ListCreateAPIView):
+    queryset = User.objects.all()
     serializer_class = CreateUserSerializer
     permission_classes = (AllowAny,)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            dict(serializer.data, token=str(user.auth_token)),
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
 
 
 class UserDetailView(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsOwnerAccountOrReadOnly,)
 
 
-class ProviderPolygonView(CreateAPIView):
-    serializer_class = CreateProviderPolygonSerializer
-    permission_classes = (AllowAny,)
+class ProviderPolygonView(ListCreateAPIView):
+    serializer_class = ProviderPolygonSerializer
+    permission_classes = (IsOwnerAccountOrReadOnly,)
 
-    def get_serializer_context(self):
-        context = super(ProviderPolygonView, self).get_serializer_context()
-        # TODO: get user instance through authentication process
-        context['user'] = get_object_or_404(User, pk=self.kwargs['pk'])
-        return context
+    def create(self, request, *args, **kwargs):
+        serializer = CreateProviderPolygonSerializer(
+            data=request.data,
+            context={'user': request.user}
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
+
+    def get_queryset(self):
+        return ProviderPolygon.objects.filter(user=self.kwargs['pk'])
 
 
 class ProviderPolygonDetailView(RetrieveUpdateDestroyAPIView):
-    queryset = ProviderPolygon.objects.all()
     serializer_class = ProviderPolygonSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsOwnerObjectOrReadOnly,)
+
+    def get_queryset(self):
+        return ProviderPolygon.objects.filter(user=self.kwargs['pk_user'])
