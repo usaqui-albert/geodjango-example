ed3c01f72d428c1f98d0e68418cf0cced68fee8a
Albert Usaqui <ausaqui@4geeks.com.ve>
2017-07-31 17:08:35 -0430

    including validations for coordinates field in the GeometrySerializer

--- polygons/serializers.py
+++ polygons/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 
 from .models import ProviderPolygon
-from .utils import get_polygon_obj, build_geometry_json_response
+from .utils import (
+    get_polygon_obj, build_geometry_json_response, lists_matches
+)
 
 
 class GeometrySerializer(serializers.Serializer):
@@ -18,7 +20,31 @@ class GeometrySerializer(serializers.Serializer):
     def validate_type(value):
         if value == 'Polygon':
             return value
-        raise serializers.ValidationError('Geometry type should be Polygon.')
+        raise serializers.ValidationError(
+            'The value of this field should be "Polygon".'
+        )
+
+    @staticmethod
+    def validate_coordinates(value):
+        if len(value) is not 1:
+            raise serializers.ValidationError(
+                'This field should follows GeoJSON object structure.'
+            )
+        coords_list = value[0]
+        if len(coords_list) < 4:
+            raise serializers.ValidationError(
+                'A Polygon should has at least 4 vertex points.'
+            )
+        for coord in coords_list:
+            if len(coord) is not 2:
+                raise serializers.ValidationError(
+                    'Every coordinate should has 2 values.'
+                )
+        if not lists_matches(coords_list[0], coords_list[-1]):
+            raise serializers.ValidationError(
+                'First and last value coordinates should match.'
+            )
+        return value
 
 
 class ProviderPolygonSerializerToWrite(serializers.ModelSerializer):
