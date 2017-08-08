12e3cbbd30f9f34baf9d86416610114f82fb510c
Albert Usaqui <ausaqui@4geeks.com.ve>
2017-08-07 23:47:09 -0430

    services documentation classes and methods

--- polygons/views.py
+++ polygons/views.py
@@ -8,10 +8,21 @@ from .serializers import ProviderPolygonWithNameSerializer
 
 
 class ProviderPolygonByLocationView(ListAPIView):
+    """Service to get the list of Polygons given a lat lng values by query
+    params, otherwise will return a list of all polygons with pagination
+
+    :accepted methods:
+        GET
+    """
     serializer_class = ProviderPolygonWithNameSerializer
     permission_classes = (AllowAny,)
 
     def get_queryset(self):
+        """Method to filter by a Point(lat, lng) and prefetching(JOIN) the
+        user owner of the polygon (provider)
+
+        :return: queryset to be used by ProviderPolygonWithNameSerializer
+        """
         queryset = ProviderPolygon.objects.all().select_related(
             'user').order_by('id')
         query_params = self.request.query_params
